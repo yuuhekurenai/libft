f9a282b6ece50243e18f410241c2cbb6aaecd941
Gabriel <yuuhekurenai@gmail.com>
2026-06-01 21:55:04 -0300

    Dia 8 - Nova função incluida utilizando a próprio libft.h

---
+++ ft_strtrim.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcelesti <gcelesti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/01 21:45:18 by gcelesti          #+#    #+#             */
+/*   Updated: 2026/06/01 21:45:18 by gcelesti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	len;
+	char	*str;
+
+	if (!s1 || !set)
+		return (NULL);
+
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+
+	len = end - start;
+
+	str = (char *)malloc(len + 1);
+	if (!str)
+		return (NULL);
+
+	ft_memcpy(str, s1 + start, len);
+	str[len] = '\0';
+
+	return (str);
+}
