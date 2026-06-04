ba078f1c47ac5eeb919d08287fe77fafa8321c50
Gabriel <yuuhekurenai@gmail.com>
2026-06-03 21:28:41 -0300

    Dia 9 - Função ft_itoa adicionada.

---
+++ ft_itoa.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcelesti <gcelesti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/03 21:02:14 by gcelesti          #+#    #+#             */
+/*   Updated: 2026/06/03 21:02:14 by gcelesti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int	count_digits(long n)
+{
+	int	count;
+
+	count = 1;
+	while (n >= 10)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	long	nb;
+	int		len;
+
+	nb = n;
+	if (nb < 0)
+		nb = -nb;
+
+	len = count_digits(nb);
+	if (n < 0)
+		len++;
+
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+
+	str[len] = '\0';
+
+	while (len > 0 && nb > 0)
+	{
+		str[--len] = (nb % 10) + '0';
+		nb /= 10;
+	}
+
+	if (n == 0)
+		str[0] = '0';
+
+	if (n < 0)
+		str[0] = '-';
+
+	return (str);
+}
