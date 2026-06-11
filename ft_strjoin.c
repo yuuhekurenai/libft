d29e9b554e16d31b727fe267c6416a9093a1f757
Gabriel <yuuhekurenai@gmail.com>
2026-06-11 12:11:10 -0300

    Dia 17 - Revisão & Correção.

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
@@ -19,12 +18,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len1;
 	size_t	len2;
 
+	if (!s1 || !s2)
+		return (NULL);
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
-	result = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	result = malloc(len1 + len2 + 1);
 	if (!result)
 		return (NULL);
 	ft_strlcpy(result, s1, len1 + 1);
-	ft_strlcat(result, s2, len2 + 1);
+	ft_strlcat(result, s2, len1 + len2 + 1);
 	return (result);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -17,16 +17,21 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
+	if (needle[0] == '\0')
+		return ((char *)haystack);
 	i = 0;
-	while (haystack[i] != '\0' && i < len)
+	while (haystack[i] && i < len)
 	{
 		j = 0;
-		while (haystack[i + j] == needle[j] && i + j < len)
+		while (needle[j]
+			&& haystack[i + j]
+			&& (i + j) < len
+			&& haystack[i + j] == needle[j])
 		{
 			j++;
-			if (needle[j + 1] == '\0')
-				return ((char *)&haystack[i]);
 		}
+		if (needle[j] == '\0')
+			return ((char *)&haystack[i]);
 		i++;
 	}
 	return (NULL);
