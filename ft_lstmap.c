794cc4b002b1ae22ed4239baec6587bdaa24c621
Gabriel <yuuhekurenai@gmail.com>
2026-06-11 12:29:45 -0300

    Dia 17 - Revisão & Correção.

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -15,23 +15,19 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
-	t_list	*new_node;
-	void	*new_content;
+	t_list	*elem;
 
-	if (!lst || !f || !del)
-		return (NULL);
-	new_lst = NULL;
+	if (!lst)
+		return (0);
+	new_lst = 0;
 	while (lst)
 	{
-		new_content = f(lst->content);
-		new_node = ft_lstnew(new_content);
-		if (!new_node)
+		if (!(elem = ft_lstnew(f(lst->content))))
 		{
-			del(new_content);
 			ft_lstclear(&new_lst, del);
-			return (NULL);
+			return (0);
 		}
-		ft_lstadd_back(&new_lst, new_node);
+		ft_lstadd_back(&new_lst, elem);
 		lst = lst->next;
 	}
 	return (new_lst);
