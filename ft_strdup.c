4597b63944187ad86ed5910005c12c40e69e7e50
Gabriel Celestino <99904833+yuuhekurenai@users.noreply.github.com>
2026-06-12 09:57:29 -0300

    Clean up includes in ft_strdup.c
    Removed unnecessary includes of stddef.h and stdlib.h.

--- ft_strdup.c
+++ ft_strdup.c
@@ -22,11 +22,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-#include <stdlib.h>
-
-#include "libft.h"
-
 #include "libft.h"
 
 char	*ft_strdup(const char *s)
