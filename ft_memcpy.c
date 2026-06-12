5bd0febea38bd12d1ca0b749d0fe87523f1df4b9
Gabriel Celestino <99904833+yuuhekurenai@users.noreply.github.com>
2026-06-12 09:56:47 -0300

    Refactor ft_memcpy implementation
    Refactor ft_memcpy to use typed pointers for better clarity.

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,14 +14,16 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
 
-	if (!dest || !src)
-		return (NULL);
+	d = (unsigned char *) dest;
+	s = (const unsigned char *) src;
 	i = 0;
 	while (i < n)
 	{
-		((char *)dest)[i] = ((char *)src)[i];
+		d[i] = s[i];
 		i++;
 	}
 	return (dest);
