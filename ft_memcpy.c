f120458e320630204325c6884c65fcae5f8b97b0
Gabriel Celestino <99904833+yuuhekurenai@users.noreply.github.com>
2026-06-15 12:15:25 -0300

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,20 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcelesti <gcelesti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 17:28:03 by gcelesti          #+#    #+#             */
+/*   Updated: 2026/05/27 17:28:03 by gcelesti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t				i;
-	unsigned char		*d;
-	const unsigned char	*s;
+	size_t			i;
+	unsigned char	*src_ptr;
+	unsigned char	*dst_ptr;
 
-	d = (unsigned char *) dest;
-	s = (const unsigned char *) src;
+	if (!dest && !src)
+		return (NULL);
 	i = 0;
+	src_ptr = (unsigned char *)src;
+	dst_ptr = (unsigned char *)dest;
 	while (i < n)
 	{
-		d[i] = s[i];
+		dst_ptr[i] = src_ptr[i];
 		i++;
 	}
 	return (dest);
